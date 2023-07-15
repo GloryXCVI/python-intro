8a0f316b5019fad1032be752436950a15575dabd
Belal Edries <136888654+baselwid@users.noreply.github.com>
2023-07-15 11:44:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,48 @@
 
 # var5 = 5+6j
 # print (type(var5))
-str1 ='hello'
-str1= 'y'+str1[1:len(str1)]
-print (str1)
-print(str1[0:2]+'mm'+str1[4:len(str1)])
+# str1 ='hello'
+# str1= 'y'+str1[1:len(str1)]
+# print (str1)
+# print(str1[0:2]+'mm'+str1[4:len(str1)])
+
+# #elif
+# if 1==2:
+#     print("1")
+# elif 1==1:
+#     print("2")
+# else:
+#     print("unkown")    
+
+# switch :
+#    case1:
+#      print("1")
+#    case2:
+#      print("2")
+#    default:
+#         print("unkown")   
+
+# num=0
+# while num<10:
+#     print(num)
+#     num=num+1
+#check an input from the user and if the word is == the value that allready stored in the varible 
+#print corect in while loop 
+#eles print wron and ask  the user to enter the word again
+
+secretword='python'
+# userinput= input('enert your word:')
+c=0
+cheak =True
+while cheak:
+    userinput= input("enter your word:")
+    c= c+1
+    if userinput== secretword:
+       print("correct")
+       break
+    else:
+       print ("wrong")
+       if c==2:
+          check=False
+          print("you have recendet")
+          break
